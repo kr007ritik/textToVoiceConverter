3d6eb8948b83c6dd4734863ed0cba1757b21e122
kr007ritik <kumar007ritik@gmail.com>
2024-02-09 00:32:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+document.getElementById("listenBtn").addEventListener("click", () => {
+  speech.text = document.getElementById("userInput").value;
+  window.speechSynthesis.speak(speech);
+});
